66e245fac65d849c90c04701d358eaa7d9547cfe
VA <duongvietanh0495@gmail.com>
2025-08-04 22:17:15 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sns-subscription-management';
+  private http = inject(HttpClient);
+  private apiRoot = 'https://2bvrsi2i93.execute-api.us-east-1.amazonaws.com';
+
+  email: string = '';
+  message: string = '';
+
+  subscribe() {
+    this.http
+      .post(`${this.apiRoot}/subscibe`, { email: this.email })
+      .subscribe((event) => {
+        console.log('Subscription successful:', event);
+      });
+    console.log('Subscribed with email:', this.email);
+  }
+
+  createEvent() {
+    this.http
+      .post(`${this.apiRoot}/create-event`, { message:  this.message})
+      .subscribe((event) => {
+        console.log('message successful:', event);
+      });
+  }
 }
